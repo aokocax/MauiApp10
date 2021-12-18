150f891656d75cd42de1b9a4948713ff8f11da7e
Ahmet Oğuz Koca <aoguzkoca@hotmail.com>
2021-12-18 18:38:18 +0300

    Add project files.

---
+++ MauiApp10/MainPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.Maui.Controls;
+
+namespace MauiApp10
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
